b0c6bfd3bacc682be42575cbf40cbd1fac58d54a
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-24 22:31:16 -0600

    completed firebase call/append

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,25 @@ var fb = new Firebase('https://xhatter.firebaseio.com/');
 	      $textInput = $form.find('input[name="chatBox"]'),
 	      name   = $nameInput.val(),
 	      text   = $textInput.val();
-
 	  $textInput.val('');
 
 	  fb.push({name: name, text: text});
 	  evt.preventDefault();
 });
+
+
+fb.on('child_added', function (snap) {
+  // fires onload once for every object in the collection,
+  // and again for additional children being added
+  var message = snap.val();
+    addChatMessage(message.name, message.text);
+});
+
+function addChatMessage(name, text) {
+  $('<div></div>')
+    .text(text)
+    .prepend(
+      $('<strong></strong>').text(name + ': ')
+    )
+    .appendTo($('.output'));
+}
